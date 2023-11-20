841637e82a65b51cc11489ed2d288905bd4f2713
AngelCer3 <aectangel@hotmail.com>
2023-11-19 23:19:36 -0600

    Proyecto anime avance dia 1

---
+++ resources/views/componente/footer.blade.php
@@ -0,0 +1,14 @@
+<div class="card mt-5 border-0 rounded-0" id="redes" style="background-color: rgb(0,0,0,0.8);">
+    <div class="card-body">
+        <div class="container text-white">
+            <div class="row justify-content-between">
+                <div class="col-md-2">
+                    <img src="{{ asset('images/Logo.webp') }}" title="Gon" class="img img-fluid mx-auto d-block">
+                </div>
+                <div class="col-12 col-md-10 align-self-center">
+                    <h4>NeoAnimeWorld</h4>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
